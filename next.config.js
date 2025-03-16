9b36372da1591f5780c782ec7f6e67ac91a447aa
jaranda-polo <yjp@jaranda.kr>
2025-03-16 06:26:30 +0000

    tempo init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+};
+
+if (process.env.NEXT_PUBLIC_TEMPO) {
+  nextConfig["experimental"] = {
+    // NextJS 14.0.1 is using this version
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+  };
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
-import '../styles/globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "../styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import { TempoInit } from "./tempo-init";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: '로컬한국어 - 현지 원어민과 실용적인 한국어 학습',
-  description: '한국 현지 생활과 여행에 필요한 실용적인 한국어를 배우는 온/오프라인 플랫폼',
+  title: "로컬한국어 - 현지 원어민과 실용적인 한국어 학습",
+  description:
+    "한국 현지 생활과 여행에 필요한 실용적인 한국어를 배우는 온/오프라인 플랫폼",
 };
 
 export default function RootLayout({
@@ -17,6 +20,8 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${inter.className} bg-gray-50`}>
+        <Script src="https://api.tempolabs.ai/proxy-asset?url=https://storage.googleapis.com/tempo-public-assets/error-handling.js" />
+        <TempoInit />
         {children}
       </body>
     </html>
